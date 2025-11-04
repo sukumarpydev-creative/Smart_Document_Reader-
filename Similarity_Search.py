5a9438eccb0f76d05ad3af558cbc61ccc7cefcb1
Sukumar <sukumar.pydev@gmail.com>
2025-11-04 09:43:39 +0530

    Similarity Search code restructured

--- Similarity_Search.py
+++ Similarity_Search.py
@@ -14,30 +14,27 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
 
-# Json to Document conversion
-json_file = './JSON Data/Chunk2Json.json'
+def run_similarity_search(json_path, query, k = 2):
+    with open(json_path, 'r', encoding='utf-8') as f:
+        file_data = json.load(f)
 
-with open(json_file, 'r', encoding='utf-8') as f:
-    file_data = json.load(f)
+    document = [ Document (
+        page_content= data['text'],
+        metadata = data['metadata'],
+    )
+    for data in file_data]
 
-document = [ Document (
-    page_content= data['text'],
-    metadata = data['metadata'],
-)
-for data in file_data]
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    vec_store = FAISS.from_documents(document, embeddings)
 
-# Setting up the embedding
-embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-
-# Setting up the FAISS store
-vec_store = FAISS.from_documents(document, embeddings)
-
-# Similarity Search from the query
-query = "What is the relationship between fungi and trees"
-search_result = vec_store.similarity_search_with_score(query, k=2)
+    return vec_store.similarity_search_with_score(query, k=2)
 
 # Displaying the result
-for i, (doc, sco) in enumerate(search_result, start=1):
-    print(f"Result [{i}] has score of {sco} and the result is :")
-    print(doc)
+if __name__ == "__main__":
+    query = "What is the relationship between fungi and trees"
+    json_file = './JSON Data/Chunk2Json.json'
+    search_result = run_similarity_search(json_file, query)
+    for i, (doc, sco) in enumerate(search_result, start=1):
+        print(f"Result [{i}] has score of {sco} and the result is :")
+        print(doc)
 
